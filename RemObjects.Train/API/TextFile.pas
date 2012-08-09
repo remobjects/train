cb2727e3cb56f4612c14e97ff7ded46cb8941e66
Jeroen Vandezande <Jeroen.Vandezande@Spathi.be>
2012-08-09 14:34:33 +0200

    Added MD5 and Textfile plugins

---
+++ RemObjects.Train/API/TextFile.pas
@@ -0,0 +1,47 @@
+﻿namespace RemObjects.Train.API;
+
+interface
+
+uses
+  RemObjects.Train,
+  RemObjects.Script.EcmaScript, 
+  RemObjects.Script.EcmaScript.Internal, 
+  System.Text,
+  System.IO;
+
+type
+  
+  [PluginRegistration]
+  TextFilePlugIn = public class(IPluginRegistration)
+  private
+  protected
+  public
+    method &Register(aServices: IApiRegistrationServices);
+    [WrapAs('textFile.write')]
+    class method WriteTextFile(aServices: IApiRegistrationServices;  ec: ExecutionContext; aFilename, aText: String);
+    [WrapAs('textFile.read')]
+    class method ReadTextFile(aServices: IApiRegistrationServices;  ec: ExecutionContext; aFilename: String): String;
+  end;
+
+implementation
+
+method TextFilePlugIn.&Register(aServices: IApiRegistrationServices);
+begin
+  var rov := aServices.RegisterObjectValue('textFile');
+  rov.AddValue('write', RemObjects.Train.MUtilities.SimpleFunction(aServices.Engine, typeOf(TextFilePlugIn), 'WriteTextFile'));
+  rov.AddValue('read', RemObjects.Train.MUtilities.SimpleFunction(aServices.Engine, typeOf(TextFilePlugIn), 'ReadTextFile'));
+end;
+
+class method TextFilePlugIn.WriteTextFile(aServices: IApiRegistrationServices; ec: ExecutionContext; aFilename: String; aText: String);
+begin
+  aFilename := aServices.ResolveWithBase(ec, aFilename);
+  File.WriteAllText(aFilename, aText);
+end;
+
+class method TextFilePlugIn.ReadTextFile(aServices: IApiRegistrationServices; ec: ExecutionContext; aFilename: String): String;
+begin
+  aFilename := aServices.ResolveWithBase(ec, aFilename);
+  result := File.ReadAllText(aFilename);
+end;
+
+end.
