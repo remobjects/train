5754b1e9361c5f6a54cc589df16abed7ae541e46
Carlo Kok <ck@remobjects.com>
2013-03-29 08:11:28 +0100

    HTTPDownload is wrong & Updated for script.

--- RemObjects.Train/API/Environment.pas
+++ RemObjects.Train/API/Environment.pas
@@ -12,7 +12,7 @@ type
     property Owner: Engine read fOwner;
     constructor(aOwner: Engine);
     method DefineOwnProperty(aName: String; aValue: PropertyValue; aThrow: Boolean): Boolean; override;
-    method GetOwnProperty(name: String; getPropertyValue: Boolean): PropertyValue; override;
+    method GetOwnProperty(name: String): PropertyValue; override;
   end;
   Environment = public class(Dictionary<String, Object>)
   private
@@ -118,7 +118,7 @@ begin
   fOwner.Environment[aName] := aValue:Value;
 end;
 
-method JVariables.GetOwnProperty(name: String; getPropertyValue: Boolean): PropertyValue;
+method JVariables.GetOwnProperty(name: String): PropertyValue;
 begin
   var lValue := fOwner.Environment[name];
   if lValue = nil then exit nil;
